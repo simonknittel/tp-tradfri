7f3c4d04d5a817204c1c549c6758dcd1752c63c4
Simon Knittel <hallo@simonknittel.de>
2020-07-12 23:05:31 +0200

    :Initial commit

---
+++ local-deploy.sh
@@ -0,0 +1,5 @@
+mkdir /mnt/c/Users/sknit/AppData/Roaming/TouchPortal/plugins/ikea-tradfri/
+
+cp entry.tp /mnt/c/Users/sknit/AppData/Roaming/TouchPortal/plugins/ikea-tradfri/
+cp ikea-tradfri.exe /mnt/c/Users/sknit/AppData/Roaming/TouchPortal/plugins/ikea-tradfri/
+cp ikea-tradfri-startcheck.exe /mnt/c/Users/sknit/AppData/Roaming/TouchPortal/plugins/ikea-tradfri/
